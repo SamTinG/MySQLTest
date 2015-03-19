7201bd6c76bf47a744c7ce931c489002cbf884ad
Sam Ting <tendousoujiooz@gmail.com>
2015-03-19 12:01:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tendo_000/mysqltest/ReadComments.java
@@ -0,0 +1,14 @@
+package com.example.tendo_000.mysqltest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReadComments extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.read_comments);
+    }
+}
